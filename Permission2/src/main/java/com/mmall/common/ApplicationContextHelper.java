04ebc0619bc95e1875000fd3917df200436f233e
Andy <2607585067@qq.com>
2024-04-02 17:52:28 +0800

    the bean tag of implementation class of ApplicationAware adds id property

--- Permission2/src/main/java/com/mmall/common/ApplicationContextHelper.java
+++ Permission2/src/main/java/com/mmall/common/ApplicationContextHelper.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-@Component("applicationContextHelper")
+//@Component("applicationContextHelper")
 public class ApplicationContextHelper implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
--- Permission2/src/main/java/com/mmall/controller/AdminController.java
+++ Permission2/src/main/java/com/mmall/controller/AdminController.java
@@ -8,8 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin")
 public class AdminController {
 
+//    @RequestMapping("index.page")
+//    public ModelAndView index() {
+//        return new ModelAndView("admin");
+//        
+//    }
+    
     @RequestMapping("index.page")
-    public ModelAndView index() {
-        return new ModelAndView("admin");
+    public String index() {
+        return "admin";
+        
     }
 }
